a65e5539568bcffb389c4263558e3fee4578924b
thomas-sc <info@schefter.net>
2024-06-11 16:55:32 +0200

    new Icons from designer, style footer

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -5,8 +5,4 @@ return [
         'provider' => \Praetorius\ViteAssetCollector\IconProvider\SvgIconProvider::class,
         'source' => 'packages/liszt_web/Resources/Private/Image/Icon/typo3.svg',
     ],
-    'liszt-portal-logo' => [
-        'provider' => \Praetorius\ViteAssetCollector\IconProvider\SvgIconProvider::class,
-        'source' => 'packages/liszt_web/Resources/Private/Image/Icon/LogoLisztPortal.svg',
-    ],
 ];
--- Resources/Private/app.js
+++ Resources/Private/app.js
@@ -1,7 +1,7 @@
 // Scss entrypoint
 import "./Scss/app.scss";
 import "./JavaScript/mainScripts.js";
-import lisztPortalLogo from './Image/Icon/LogoLisztPortal.svg?url'
+// import lisztPortalLogo from './Image/Icon/LogoLisztPortal.svg?url'
 
 // import typo3Logo from './Image/Icon/typo3.svg?url'
 
