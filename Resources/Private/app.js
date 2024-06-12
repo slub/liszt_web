3552a727f261c57b92d1fb24df88790210cc315e
thomas-sc <info@schefter.net>
2024-06-12 15:43:17 +0200

    first styling for main navigation

--- Resources/Private/app.js
+++ Resources/Private/app.js
@@ -5,8 +5,3 @@ import "./JavaScript/mainScripts.js";
 
 // import typo3Logo from './Image/Icon/typo3.svg?url'
 
-
-// TYPO3.lang contains all available inline labels
-//console.log("Say hi! " + TYPO3.lang["liszt_web.welcome"]);
-
-
