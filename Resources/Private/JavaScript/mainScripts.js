b6afc9f2b5d2d589a8c6695efb20747adde94d36
thomas-sc <info@schefter.net>
2024-07-08 17:07:17 +0200

    first variant of intersection observer for menu_section

--- Resources/Private/JavaScript/mainScripts.js
+++ Resources/Private/JavaScript/mainScripts.js
@@ -18,3 +18,41 @@ const prvs = new Parvus({
  lightboxIndicatorIcon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" aria-hidden="true" fill="none"  stroke-width="1.5" stroke="currentColor" focusable="false"> <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" /></svg>',
 })
 
+
+// Intersection Observer for highlighting active items in Sticky Menu
+// ToDo: Bugfix the selection of Elements if there is a long list of Sections with small heights
+const targetLinks = document.querySelectorAll(('[data-target-id]'));
+
+// start only if targetLinks exists
+if (targetLinks.length > 0) {
+
+  const options = {
+    rootMargin: `0px`,
+    threshold: 1
+  }
+
+  const targetIds = Array.from(targetLinks).map(element => element.getAttribute('data-target-id'));
+  const targets = targetIds.map(id => document.getElementById(id));
+// console.log(targetIds);
+
+  const onIntersect = (entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        console.log(entry.target.id, 'is intersecting')
+        targetLinks.forEach(link => {
+          if(link.getAttribute('data-target-id') === entry.target.id){
+            link.classList.add('active'); }
+          else {
+            link.classList.remove('active');
+          } });
+      } else {
+        //  console.log("not intersecting", entry);
+      }
+    })
+  }
+
+  const observer = new IntersectionObserver(onIntersect, options);
+  targets.forEach(target => observer.observe(target));
+
+}
+
