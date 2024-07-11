955654bb9ad18f8ae3bf64ddab6fbca35f453666
Thomas Schefter <listen@schefter.net>
2024-07-11 11:18:07 +0200

    try to modify rte preset to accept q tags

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,7 +8,7 @@ defined('TYPO3') or die();
 $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['vite_asset_collector']['defaultManifest'] = 'EXT:liszt_web/Resources/Public/Vite/.vite/manifest.json';
 
 // Include custom RTE config
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:liszt_web/Configuration/RTE/RteDefaultPreset.yaml';
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:liszt_web/Configuration/RTE/RteLisztFullPreset.yaml';
 
 
 if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('iconpack')) {
